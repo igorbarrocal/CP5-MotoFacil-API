81af40bd38c0742ada69892ecb9c209dfe73777b
igorbarrocal <igorbarrocal@hotmail.com>
2025-10-16 13:49:14 -0300

    feat: add MongoDbContext for MongoDB collections

---
+++ MotoFacil-API/Infrastructure/Data/MongoDbContext.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Driver;
+using MotoFacilAPI.Domain.Entities;
+
+namespace MotoFacilAPI.Infrastructure.Data
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(string connectionString, string dbName)
+        {
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase(dbName);
+        }
+
+        public IMongoCollection<Usuario> Usuarios => _database.GetCollection<Usuario>("Usuarios");
+        public IMongoCollection<Moto> Motos => _database.GetCollection<Moto>("Motos");
+        public IMongoCollection<Servico> Servicos => _database.GetCollection<Servico>("Servicos");
+    }
+}
